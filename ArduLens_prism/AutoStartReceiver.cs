655ab45131e0602c97207fde2aa6c7cb14bc1f3a
Andrew Pstvt <AndrewPstvt@yandex.ru>
2022-06-30 08:40:28 +0300

    Добавьте файлы проекта.

---
+++ ArduLens_prism/AutoStartReceiver.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using ArduLens_prism.Services;
+using Prism.DryIoc;
+using MyApp = Android.App.Application;
+
+namespace ArduLens_prism
+{
+    [BroadcastReceiver(Enabled = true, Exported = true, DirectBootAware = true)]
+    [IntentFilter(new[] { Intent.ActionBootCompleted, Intent.ActionLockedBootCompleted, "android.intent.action.QUICKBOOT_POWERON" })]
+    class AutoStartReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+        //    AlarmManager alarmManager = (AlarmManager)MyApp.Context.GetSystemService(Context.AlarmService);
+        //    Intent i = new Intent(MyApp.Context, Java.Lang.Class.FromType(typeof(StepCounter.AlarmReceiver)));
+        //    PendingIntent pending = PendingIntent.GetBroadcast(MyApp.Context, 1, i, PendingIntentFlags.UpdateCurrent);
+        //    alarmManager.SetRepeating(AlarmType.RtcWakeup, Java.Lang.JavaSystem.CurrentTimeMillis() + 2500, AlarmManager.IntervalFifteenMinutes, pending);
+        }
+    }
+}
